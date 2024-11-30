dc97254142ef0550988e618d4539195ce3bf5f35
elcarp <lise.carpenter@gmail.com>
2024-11-30 15:37:35 +0700

    adjust component height

--- components/hero/index.tsx
+++ components/hero/index.tsx
@@ -67,7 +67,7 @@ export default function HeroParallax({
   return (
     <div
       ref={ref}
-      className='h-[300vh] py-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]'>
+      className='h-[300vh] pt-[17rem] pb-40 overflow-hidden  antialiased relative flex flex-col self-auto [perspective:1000px] [transform-style:preserve-3d]'>
       <Header />
       <motion.div
         style={{
@@ -146,7 +146,7 @@ export const ProductCard = ({
         y: -20,
       }}
       key={product.title}
-      className='group/product h-96 w-[30rem] relative flex-shrink-0'>
+      className='group/product h-64 w-[30rem] relative flex-shrink-0'>
       <Link
         href={product.link}
         rel='noreferrer'
