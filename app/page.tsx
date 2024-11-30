00c364ebef6918ffd18680d8815fd936d0b0c869
elcarp <lise.carpenter@gmail.com>
2024-11-30 13:21:02 +0700

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // import { FeaturesSection } from '~components/features'
+import Footer from '~components/footer'
 import Hero from '~components/hero'
 
 export default function Home() {
@@ -102,6 +103,7 @@ export default function Home() {
     <>
       <Hero products={products} />
       {/* <FeaturesSection /> */}
+      <Footer />
     </>
   )
 }
